1a6ad2dc4e5786e034526ee7a055f7a3be0ae644
DerTeufel <dominik-kassel@gmx.de>
2012-10-04 20:21:40 +0200

    Revert "Framework: NavBar transparency (1/2)"
    This reverts commit 8e463dffd5f4139d5f405a80acf36d22b0ef3a22.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -310,8 +310,6 @@ public class PhoneStatusBar extends BaseStatusBar {
                     Settings.System.HIGH_END_GFX_ENABLED), false, this);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.STATUS_BAR_TRANSPARENCY), false, this);
-            resolver.registerContentObserver(Settings.System.getUriFor(
-                    Settings.System.NAV_BAR_TRANSPARENCY), false, this);
             update();
         }
 
@@ -330,7 +328,6 @@ public class PhoneStatusBar extends BaseStatusBar {
             mHighEndGfx = Settings.System.getInt(resolver,
                     Settings.System.HIGH_END_GFX_ENABLED, 0) != 0;
             setStatusBarParams(mStatusBarView);
-            setNavigationBarParams();
         }
     }
 
@@ -820,12 +817,6 @@ public class PhoneStatusBar extends BaseStatusBar {
         mNavigationBarView.reorient();
         mNavigationBarView.setListener(mRecentsClickListener,mRecentsPanel, mHomeSearchActionListener);
         updateSearchPanel();
-    }
-
-    protected void setNavigationBarParams(){
-        int opacity = Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.NAV_BAR_TRANSPARENCY, 100);
-        mNavigationBarView.getBackground().setAlpha(Math.round((opacity * 255) / 100));
     }
 
     // For small-screen devices (read: phones) that lack hardware navigation buttons
@@ -2715,7 +2706,6 @@ public class PhoneStatusBar extends BaseStatusBar {
             mCurrentTheme = (CustomTheme)newTheme.clone();
             recreateStatusBar();
             setStatusBarParams(mStatusBarView);
-            setNavigationBarParams();
         } else {
 
             if (mClearButton instanceof TextView) {

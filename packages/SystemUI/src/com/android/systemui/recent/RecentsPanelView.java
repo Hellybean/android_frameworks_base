dab2a19e5a71c92bfdeb291173dba48d1652b449
DerTeufel <dominik-kassel@gmx.de>
2012-10-18 21:19:11 +0200

    testing: another kill all button
    Change-Id: Ib7cc5f00ace9e206bd6cc4cac04c7b8234be27c0

--- packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
+++ packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
@@ -33,6 +33,7 @@ import android.graphics.Shader.TileMode;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.os.Handler;
 import android.os.RemoteException;
 import android.os.ServiceManager;
 import android.provider.Settings;
@@ -53,9 +54,11 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.BaseAdapter;
 import android.widget.FrameLayout;
+import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
 import android.widget.ImageView.ScaleType;
 import android.widget.PopupMenu;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.content.pm.PackageManager;
 
@@ -67,6 +70,7 @@ import com.android.systemui.statusbar.tablet.StatusBarPanel;
 import com.android.systemui.statusbar.tablet.TabletStatusBar;
 
 import java.util.ArrayList;
+import java.util.ConcurrentModificationException;
 
 public class RecentsPanelView extends FrameLayout implements OnItemClickListener, RecentsCallback,
         StatusBarPanel, Animator.AnimatorListener, View.OnTouchListener {
@@ -78,6 +82,7 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
     private View mRecentsScrim;
     private View mRecentsNoApps;
     private ViewGroup mRecentsContainer;
+    private Object mScrollView;
     private StatusBarTouchProxy mStatusBarTouchProxy;
 
     private boolean mShowing;
@@ -88,11 +93,14 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
     private Choreographer mChoreo;
     OnRecentsPanelVisibilityChangedListener mVisibilityChangedListener;
 
+    ImageView mClearRecents;
     ImageView mPlaceholderThumbnail;
     View mTransitionBg;
     boolean mHideRecentsAfterThumbnailScaleUpStarted;
 
     private Button mRecentsTaskManagerButton;
+
+    Handler mTaskHandler;
 
     private RecentTasksLoader mRecentTasksLoader;
     private ArrayList<TaskDescription> mRecentTaskDescriptions;
@@ -233,6 +241,7 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
     public RecentsPanelView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
         mContext = context;
+        mTaskHandler = new Handler();
         updateValuesFromResources();
 
         TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.RecentsPanelView,
@@ -320,6 +329,12 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
             // if there are no apps, either bring up a "No recent apps" message, or just
             // quit early
             boolean noApps = !mFirstScreenful && (mRecentTaskDescriptions.size() == 0);
+
+            // if no apps found, we just hide the "Clear" button as it's not needed
+            if(mClearRecents != null){
+                mClearRecents.setVisibility(noApps ? View.GONE : View.VISIBLE);
+            }
+
             if (mRecentsNoApps != null) {
                 mRecentsNoApps.setAlpha(1f);
                 mRecentsNoApps.setVisibility(noApps ? View.VISIBLE : View.INVISIBLE);
@@ -467,6 +482,8 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
         mFitThumbnailToXY = res.getBoolean(R.bool.config_recents_thumbnail_image_fits_to_xy);
     }
 
+    protected static ArrayList<View> mViewContainer = new ArrayList();
+
     @Override
     protected void onFinishInflate() {
         super.onFinishInflate();
@@ -489,6 +506,45 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
         mChoreo = new Choreographer(this, mRecentsScrim, mRecentsContainer, mRecentsNoApps, this);
         mHighEndGfx = Settings.System.getInt(mContext.getContentResolver(),
 	                Settings.System.HIGH_END_GFX_ENABLED, 0) != 0;
+
+        mClearRecents = (ImageView) findViewById(R.id.recents_clear);
+        if (mClearRecents != null){
+            mClearRecents.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    new Thread(new Runnable(){
+                        public void run(){
+                            if(mScrollView instanceof ScrollView){
+                                ((ScrollView) mScrollView).smoothScrollTo(0,0);
+                            } else if(mScrollView instanceof HorizontalScrollView) {
+                                ((HorizontalScrollView) mScrollView).smoothScrollTo(0,0);
+                            }
+
+                            try{
+                                for (final View child : mViewContainer){
+                                    mTaskHandler.post(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            mRecentsContainer.removeViewInLayout(child);
+                                        }
+                                    });
+
+                                    // Add a small delay before of removing next app
+                                    Thread.sleep(150);
+                                }
+                            } catch (ConcurrentModificationException e){
+                                // User pressed back key before animation finished. This is not
+                                // such a good idea, and we can't deal with it on any other way,
+                                // so we just interrupt the process instead of crashing.
+                            } catch (InterruptedException ie){
+                                // User will see the app fading instantly after the previous
+                                // one. This will probably never happen
+                            }
+                        }
+                    }).start();
+                }
+            });
+        }
 
         if (mRecentsScrim != null) {
             Display d = ((WindowManager)mContext.getSystemService(Context.WINDOW_SERVICE))
@@ -790,7 +846,8 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
         ActivityOptions opts = ActivityOptions.makeDelayedThumbnailScaleUpAnimation(
                 holder.thumbnailViewImage, bm, 0, 0,
                 new ActivityOptions.OnAnimationStartedListener() {
-                    @Override public void onAnimationStarted() {
+                    @Override
+                    public void onAnimationStarted() {
                         mThumbnailScaleUpStarted = true;
                         if (!mHighEndGfx) {
                             mPlaceholderThumbnail.setVisibility(INVISIBLE);
@@ -849,10 +906,14 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
             hide(false);
         }
 
-        // Currently, either direction means the same thing, so ignore direction and remove
-        // the task.
         final ActivityManager am = (ActivityManager)
                 mContext.getSystemService(Context.ACTIVITY_SERVICE);
+        removeTask(am, ad);
+    }
+
+    private void removeTask(ActivityManager am, TaskDescription ad){
+        // Currently, either direction means the same thing, so ignore direction and remove
+        // the task.
         if (am != null) {
             am.removeTask(ad.persistentTaskId, ActivityManager.REMOVE_TASK_KILL_PROCESS);
 
@@ -877,6 +938,10 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
         } else {
             return super.onInterceptTouchEvent(ev);
         }
+    }
+
+    public void setScrollView(Object scrollView){
+        mScrollView = scrollView;
     }
 
     public void handleLongPress(
@@ -914,7 +979,6 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
         popup.show();
     }
 
-
     private void opentaskmanager() {
     	PackageManager pm = getContext().getPackageManager();
     	Intent appStartIntent = pm.getLaunchIntentForPackage("com.eolwral.osmonitor");
@@ -924,4 +988,11 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
     	}
     }
 
+    public void addContainer(View container){
+        mViewContainer.add(container);
+    }
+
+    public void clear(){
+        mViewContainer.clear();
+    }
 }

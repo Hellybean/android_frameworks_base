777c0fb1f1fa2aec7e8fa6ccfe7ae96d32adf1f2
DerTeufel <dominik-kassel@gmx.de>
2012-10-23 09:24:31 +0200

    don't code when i should sleep
    Change-Id: Ia0bef2e7f8cedc3b7233cc6f835b15481f96e536

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2976,19 +2976,6 @@ public final class Settings {
          */
         public static final String POWER_MENU_NAV_CONTROLS_ENABLED = "power_menu_nav_controls_enabled";
 
-
-        /**
-         * Whether power menu airplane toggle is enabled
-         * @hide
-         */
-        public static final String POWER_MENU_AIRPLANE_ENABLED = "power_menu_airplane_enabled";
-
-        /**
-         * Whether power menu silent mode is enabled
-         * @hide
-         */
-        public static final String POWER_MENU_SILENT_ENABLED = "power_menu_silent_enabled";
-
         /**
          * Whether to enable custom rebindings of the actions performed on
          * certain key press events.

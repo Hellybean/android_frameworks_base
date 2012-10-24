fbf7805d8149511958c762a83320ae4e86bfe144
DerTeufel <dominik-kassel@gmx.de>
2012-10-24 10:05:04 +0200

    custom blackberry 10 lockscreen...not working properly
    Change-Id: I0de421aa3df811c9b4e4eef4acfa6907ced077ad

--- services/java/com/android/server/wm/WindowAnimator.java
+++ services/java/com/android/server/wm/WindowAnimator.java
@@ -287,17 +287,7 @@ public class WindowAnimator {
                         mService.mFocusMayChange = true;
                     }
                     if (win.isReadyForDisplay()) {
-                        if (Settings.System.getInt(mContext.getContentResolver(),
-                                    Settings.System.LOCKSCREEN_SEE_THROUGH, 0) != 0 ||
-                                    nowAnimating) {
-                            if (winAnimator.mAnimationIsEntrance) {
-                                mForceHiding = KEYGUARD_ANIMATING_IN;
-                            } else {
-                                mForceHiding = KEYGUARD_ANIMATING_OUT;
-                            }
-                        } else {
-                            mForceHiding = KEYGUARD_SHOWN;
-                        }
+                            mForceHiding = KEYGUARD_NOT_SHOWN;
                     }
                     if (WindowManagerService.DEBUG_VISIBILITY) Slog.v(TAG,
                             "Force hide " + mForceHiding

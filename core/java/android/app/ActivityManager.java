dda51d8d67ebe96da47d9b576ee75cf8baf76b07
Chester Kener <Cl3Kener@gmail.com>
2014-02-01 17:46:02 -0500

    Fix Netflix Crash
    * And other things.  I found the issue with Netflix not working on our ROM.  I think this can fix several apps not working so hopefully it did.  :)
    
    Signed-off-by: Chester Kener <Cl3Kener@gmail.com>

--- core/java/android/app/ActivityManager.java
+++ core/java/android/app/ActivityManager.java
@@ -51,7 +51,6 @@ import android.text.TextUtils;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.util.Slog;
-import android.view.HardwareRenderer;
 
 import java.io.FileDescriptor;
 import java.io.FileOutputStream;
@@ -285,15 +284,9 @@ public class ActivityManager {
     /** @hide Process is being cached for later use and is empty. */
     public static final int PROCESS_STATE_CACHED_EMPTY = 13;
 
-    private static boolean NOPE;
-
     /*package*/ ActivityManager(Context context, Handler handler) {
         mContext = context;
         mHandler = handler;
-
-        NOPE = (isLowRamDeviceStatic() ||
-                !HardwareRenderer.isAvailable()      ||
-                Resources.getSystem().getBoolean(com.android.internal.R.bool.config_avoidGfxAccel));
     }
 
     /**

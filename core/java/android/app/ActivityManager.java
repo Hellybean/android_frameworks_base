d3cdf0a45d97571b58836e36259aaf6d58252763
Chester Kener <Cl3Kener@gmail.com>
2014-02-11 15:59:05 -0500

    Remove Unneeded Stuff
    Signed-off-by: Chester Kener <Cl3Kener@gmail.com>

--- core/java/android/app/ActivityManager.java
+++ core/java/android/app/ActivityManager.java
@@ -451,25 +451,12 @@ public class ActivityManager {
         return "true".equals(SystemProperties.get("ro.config.low_ram", "false"));
     }
 
-    /** @hide */
-    public static boolean forceHighEndGfx() {
-        return "1".equals(SystemProperties.get("persist.sys.highendgfx", deviceIsHighEndGfx() ?
-                "1" : "0"));
-    }
-
-    /** @hide */
-    public static boolean deviceIsHighEndGfx() {
-        return !isLowRamDeviceStatic() &&
-                !Resources.getSystem().getBoolean(com.android.internal.R.bool.config_avoidGfxAccel);
-    }
-
     /**
      * Used by persistent processes to determine if they are running on a
      * higher-end device so should be okay using hardware drawing acceleration
      * (which tends to consume a lot more RAM).
      * @hide
      */
-    static public boolean isHighEndGfx() {
         return (!isLowRamDeviceStatic() &&
                 !Resources.getSystem().getBoolean(com.android.internal.R.bool.config_avoidGfxAccel))
                 || isForcedHighEndGfx();
